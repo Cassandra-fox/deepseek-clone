1bf98c9cce94ae3060bc08b8f054816f2ceec4cb
Cassandra <gaosurfing@foxmail.com>
2025-12-01 16:41:10 +0800

    前端加数据库接入

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 'use client'
 
 import Image from "next/image";
-import { useState } from "react";
+import { useState } from "react"
+import EmailIcon from '@mui/icons-material/Email'
+import SendIcon from '@mui/icons-material/Send';
 
 export default function Home() {
   const [input, setInput] = useState("")
   const [model, setModel] = useState("deepseek-v3")
+  const handleSubmit = ()=>{}
 
   return (
     <div className="h-screen flex flex-col items-center">
@@ -15,17 +18,25 @@ export default function Home() {
           有什么可以帮您的吗？
         </p>
         <div className="flex flex-col items-center justify-center mt-4 shadow-lg border-[1px] border-gray-300 h-32 rounded-lg">
-          <textarea className="w-full rounded-lg p-3 h-30 focus:outline-none" value={input} onChange={(e)=>setInput(e.target.value)}>
 
+          <textarea className="w-full rounded-lg p-3 h-30 focus:outline-none" value={input} onChange={(e)=>setInput(e.target.value)}>
           </textarea>
-            <div className={`"flex flex-row items-center justify-between w-full h-12 mb-2">
-              <div className="flex flex-row items-center justify-center rounded-lg border-[1px] px-2 py-2 ml-2 cursor-pointer ${model=== 'deepseek-r1'? "border-blue-300 bg-blue-200" :"border-gray-300"}`}>
-                <p className="text-sm">
-                  深度思考（R1）
-                </p>
+
+          <div className="flex flex-row items-center justify-between w-full h-12 mb-2">
+              <div>
+                <div className={`flex flex-row items-center justify-center rounded-lg border-[1px] px-2 py-1 ml-2 cursor-pointer ${model=== 'deepseek-r1'? 'border-blue-300 bg-blue-200':'border-gray-300'}`}>
+                  <p className="text-sm ">
+                    深度思考(R1)
+                  </p>
+                </div>
               </div>
+            
+            <div className="flex items-center justify-center border-2 mr-4 border-black p-1 rounded-full" onClick={handleSubmit}>
+              <SendIcon></SendIcon>
             </div>
         </div>
+        </div>
       </div>
-  );
+    </div>
+  )
 }
